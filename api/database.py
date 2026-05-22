71bab6b10519893dc10f3e0d5f9a460c88c1772d
Luhsti <lucas_cavalcanti@id.uff.br>
2026-05-22 13:04:10 -0300

    conclusão fastAPI - com paginação

--- api/database.py
+++ api/database.py
@@ -5,9 +5,6 @@ DATABASE_URL = "postgresql://postgres:admin@localhost:32768/postgres"
 
 engine = create_engine(DATABASE_URL)
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
 def get_session():
     with Session(engine) as session:
         yield session
